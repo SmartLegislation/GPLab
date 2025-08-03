244d88cb4fbd563dbb2c7d3ceec655c6d7843f39
SmartLegislation <3585646245@qq.com>
2025-08-03 14:03:47 +0800

    feat: 初始化项目基础架构和配置文件
    refactor: 重构子系统基类和工具模块
    
    docs: 添加各子系统的详细说明文档
    
    build: 添加项目依赖和配置文件
    
    chore: 设置git忽略规则和项目目录结构

--- src/subsystems/base.py
+++ src/subsystems/base.py
@@ -1,11 +1,4 @@
-'''
-Author: FoeverTree 11965818+cromwell_mao@user.noreply.gitee.com
-Date: 2025-05-13 11:34:34
-LastEditors: FoeverTree 11965818+cromwell_mao@user.noreply.gitee.com
-LastEditTime: 2025-05-13 11:41:42
-FilePath: \GPLab\src\subsystems\base.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 from abc import ABC, abstractmethod
 from typing import Dict, Any, List, Optional
 
